2576a28072b964a1e28edf50896f32bbee52d3ab
brianmarti1994 <brianmarti1994@gmail.com>
2020-05-03 15:04:36 +0300

    SPA website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { CountComponent } from './count/count.component';
+import { ServiceComponent } from './service/service.component';
+import { FeaturesComponent } from './features/features.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    AboutComponent,
+    GalleryComponent,
+    TeamComponent,
+    ContactComponent,
+    CountComponent,
+    ServiceComponent,
+    FeaturesComponent,
+    TestimonialComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
